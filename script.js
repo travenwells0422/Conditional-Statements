96cab35b0dcab3b8e24617c1d73b5b7c4afe5b93
travenwells0422 <travendwellsSoftwareDeveloper@outlook.com>
2023-02-01 22:19:35 -0500

    pushing up to inlineStyling

--- script.js
+++ script.js
@@ -40,12 +40,16 @@ function solveOnClick() {
     let value = parseInt(a) + parseInt(b);
 
     const h2Element = document.createElement("h2");
+    h2Element.style.paddingTop = "40px";
     section.append(h2Element);
 
     const image = document.createElement("img");
     image.style.width = "300px";
     image.style.height = "300px";
+    image.style.marginBottom = "40px";
     image.style.objectFit = "contain";
+    image.style.borderStyle = "solid";
+    image.style.borderWidth = "thick";
     image.style.borderRadius = "3rem";
     section.append(image);
 
@@ -73,3 +77,28 @@ document
 function reloadOnClick() {
     location.reload();
 }
+
+// Inline Styling
+const body = document.querySelector("body");
+body.style.backgroundColor = "lightGray";
+body.style.margin = "10px";
+
+// Header Inline - Styling
+header.style.height = "100px";
+header.style.backgroundColor = "orange";
+header.style.borderStyle = "solid";
+header.style.borderWidth = "thick";
+header.style.borderRadius = "2rem";
+header.style.textAlign = "center";
+
+h1Element.style.paddingTop = "10px";
+
+// Main Inline - Styling
+main.style.marginTop = "40px";
+main.style.textAlign = "center";
+
+// Section Inline - Styling
+section.style.marginTop = "20px";
+section.style.textAlign = "center";
+section.style.background = "gray";
+section.style.borderRadius = "1rem";
